d93c9e6eb08627927fb15ffe80e4ec3fdfb36b5c
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2025-04-10 21:03:50 -0300

    Fead: Adicionado os campos que serão vinculados ao banco de dados

---
+++ skeleton-main/skeleton/skeleton-ejb/src/main/java/br/com/skeleton/business/entity/Produto.java
@@ -0,0 +1,45 @@
+package br.com.skeleton.business.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+
+@Entity
+@Table(name = "equipamentos")
+public class Produto implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "nome", nullable = false)
+  private String nome;
+
+  @Column(name = "descricao", nullable = false)
+  private String descricao;
+
+  @Column(name = "data", nullable = false)
+  private LocalDate data;
+
+  @Column(name = "valor", nullable = false, precision = 10, scale = 2)
+  private BigDecimal valor;
+
+  @Column(name = "validade", nullable = false)
+  private LocalDate validade;
+
+  @Column(name = "status", nullable = false, columnDefinition = "VARCHAR(20) DEFAULT 'ativo'")
+  private String status = "ativo";
+
+}
